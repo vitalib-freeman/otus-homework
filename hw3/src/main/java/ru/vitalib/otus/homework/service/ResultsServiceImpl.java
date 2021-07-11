f213086b439f0fef8a4a0c6ec1df88700f715283
Vitaliy Baranov <vigriban@yandex.ru>
2021-07-11 13:29:16 +0300

    hw3 add translation

--- hw3/src/main/java/ru/vitalib/otus/homework/service/ResultsServiceImpl.java
+++ hw3/src/main/java/ru/vitalib/otus/homework/service/ResultsServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.vitalib.otus.homework.service;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import ru.vitalib.otus.homework.model.Score;
 
@@ -9,14 +10,16 @@ public class ResultsServiceImpl implements ResultsService {
   private static final String TEST_RESULT_MESSAGE = "Result: %s pass";
 
   private final InputOutputService inputOutputService;
+  private final MessageSource messageSource;
 
-  public ResultsServiceImpl(InputOutputService inputOutputService) {
+  public ResultsServiceImpl(InputOutputService inputOutputService, MessageSource messageSource) {
     this.inputOutputService = inputOutputService;
+    this.messageSource = messageSource;
   }
 
   @Override
   public void provideResult(Score score, String userName) {
-    inputOutputService.write(String.format(SCORE_MESSAGE, userName, score.getQuestionsWithCorrectAnswer(),
+    inputOutputService.write(String.format(messageSource.getMessage(""), userName, score.getQuestionsWithCorrectAnswer(),
       score.getTotalQuestions()));
     inputOutputService.write(String.format(TEST_RESULT_MESSAGE, score.isHasPass() ? "" : "not"));
   }
--- hw3/src/main/java/ru/vitalib/otus/homework/service/UserServiceImpl.java
+++ hw3/src/main/java/ru/vitalib/otus/homework/service/UserServiceImpl.java
@@ -1,19 +1,24 @@
 package ru.vitalib.otus.homework.service;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class UserServiceImpl implements UserService {
   private static final String WELCOME_MESSAGE = "Hello, what is your name?";
   private final InputOutputService inputOutputService;
+  private final MessageSource messageSource;
 
-  public UserServiceImpl(InputOutputService inputOutputService) {
+  public UserServiceImpl(InputOutputService inputOutputService, MessageSource messageSource) {
     this.inputOutputService = inputOutputService;
+    this.messageSource = messageSource;
   }
 
   @Override
   public String getUserName() {
-    inputOutputService.write(WELCOME_MESSAGE);
+    inputOutputService.write(messageSource.getMessage("user.greeting", null, Locale.getDefault()));
     return inputOutputService.read();
   }
 }
